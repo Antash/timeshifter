98368f30391f35fa7ab147614f956f3172eda507
anton <anton@104cdb4f-c8c8-40c2-b96b-405fcb135c29>
2011-03-04 20:04:09 +0000

    nativ win32 c++ dll project added
    I think it would be better to use native c++ instead of c++/CLI
    (just use dllimport attribute to import dll functions)

--- test/Form1.cs
+++ test/Form1.cs
@@ -4,14 +4,25 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
-using wintracer;
 
 namespace test
 {
     public partial class Form1 : Form
     {
+		[DllImport("winhook.dll")]
+		public static extern int getActWindowPID();
+		[DllImport("winhook.dll", CharSet = CharSet.Ansi, ExactSpelling = true,
+		   CallingConvention = CallingConvention.Cdecl,
+		    EntryPoint="getActWindowPID")]
+		//[return: MarshalAs(UnmanagedType.LPWStr)]
+		public static extern IntPtr getActWindowProcName();
+		[DllImport("winhook.dll")]
+		//[return: MarshalAs(UnmanagedType.LPWStr)]
+		public static extern string getActWindowCaption();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,11 +30,15 @@ namespace test
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Text = WinTracerMain.getActWindowPID().ToString();
+            label1.Text = getActWindowPID().ToString();
             unsafe
             {
-                label3.Text = charToString(WinTracerMain.getActWindowProcName());
-                label4.Text = charToString(WinTracerMain.getActWindowCaption());
+            	//getActWindowProcName();
+            	//label3.Text = charToString(getActWindowProcName());
+            	//label4.Text = charToString(getActWindowCaption());
+            	label3.Text = Marshal.PtrToStringAnsi(getActWindowProcName());
+            	//label4.Text = Marshal.PtrToStringAnsi(getActWindowCaption());
+
             }
         }
 
---
+++ winhook/main.cpp
@@ -0,0 +1,62 @@
+#include <windows.h>
+#include <tlhelp32.h>
+
+extern "C"
+{
+	__declspec(dllexport) int getActWindowPID()
+	{
+		HWND wnd = GetForegroundWindow();
+		DWORD processId = -1;
+		if (wnd)
+		{
+			GetWindowThreadProcessId(wnd, &processId);
+		}
+		return processId;
+	}
+
+	__declspec(dllexport) LPTSTR getActWindowCaption()
+	{
+		HWND wnd = GetForegroundWindow();
+		LPWSTR windowText;
+		if (wnd)
+		{
+			int buffLen = GetWindowTextLength(wnd) + 1;
+			windowText = new wchar_t[buffLen];
+			GetWindowText(wnd, windowText, buffLen);
+		}
+		else
+		{
+			windowText = L"no active process";
+		}
+		return windowText;
+	}
+
+	__declspec(dllexport) LPWSTR getActWindowProcName()
+	{
+		DWORD processId = getActWindowPID();
+
+		PROCESSENTRY32 pe;
+		HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+		LPWSTR pName;
+
+		if(!hSnapshot || processId == -1)
+			pName = L"no active process";
+		else
+		{
+			// Initialize size in structure
+			pe.dwSize = sizeof(PROCESSENTRY32);
+
+			for(int i = Process32First(hSnapshot, &pe); i; i=Process32Next(hSnapshot, &pe))
+			{
+				if(pe.th32ProcessID == processId)
+				{
+					pName = pe.szExeFile;
+					break;
+				}
+			}
+		}
+		CloseHandle(hSnapshot); // Done with this snapshot. Free it
+		return pName;
+	}
+}
