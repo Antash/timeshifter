e84706cb5778e237f0dd8049c496bd60dab7df1f
anton <anton@104cdb4f-c8c8-40c2-b96b-405fcb135c29>
2011-03-12 12:34:02 +0000

    programming tutorial WIP

---
+++ CsTutorial/A.cs
@@ -0,0 +1,64 @@
+﻿namespace CsTutorial
+{
+	public class A
+	{
+		public event DelegateA AEvent;
+
+		protected void InvokeAEvent(DelegateAArgs args)
+		{
+			DelegateA handler = AEvent;
+			if (handler != null) handler(this, args);
+		}
+
+		private int _privV;
+		protected int ProtV;
+		public int PubV;
+		public static int PubStatV;
+
+		public A()
+		{
+			_privV = 0;
+			ProtV = 0;
+		}
+
+		public A(int a)
+		{
+			_privV = a;
+			ProtV = a;
+		}
+
+		public int PrivV
+		{
+			get { return _privV; }
+			set { _privV = value; }
+		}
+
+		internal void EInvoke()
+		{
+			InvokeAEvent(new DelegateAArgs());
+		}
+
+		protected int FProtected()
+		{
+			return _privV / 10;
+		}
+
+		protected static void FStatic()
+		{
+		}
+	}
+
+	public class DelegateAArgs
+	{
+		public int E;
+		public DelegateAArgs()
+		{
+		}
+		public DelegateAArgs(int e)
+		{
+			E = e;
+		}
+	}
+
+	public delegate void DelegateA(object sender, DelegateAArgs args);
+}
---
+++ CsTutorial/B.cs
@@ -0,0 +1,16 @@
+﻿namespace CsTutorial
+{
+	sealed class B : A
+	{
+		private double _b;
+		public B()
+		{
+		}
+
+		public B(int a, double b)
+			: base(a)
+		{
+			_b = b;
+		}
+	}
+}
