3b82cc3ebe13ac97c64835029d66c7f26d05cc5c
anton <anton@104cdb4f-c8c8-40c2-b96b-405fcb135c29>
2011-03-24 14:40:03 +0000

    gui wpf project added by aleksey

---
+++ GUI/MainControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+	/// <summary>
+	/// Interaction logic for MainControl.xaml
+	/// </summary>
+	public partial class MainControl
+	{
+		public MainControl()
+		{
+			this.InitializeComponent();
+		}
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+	}
+}
