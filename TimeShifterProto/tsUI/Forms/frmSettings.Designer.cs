45cedf2b2da9991a8d829834dc952f74684c353d
Anton Ashmarin <aashmarin@gmail.com>
2011-04-01 11:00:51 +0000

    prototype trunk creation WIP

--- TimeShifterProto/tsUI/Forms/frmSettings.Designer.cs
+++ TimeShifterProto/tsUI/Forms/frmSettings.Designer.cs
@@ -28,33 +28,34 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
+			this.bAutostart = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
-			// button1
+			// bAutostart
 			// 
-			this.button1.Location = new System.Drawing.Point(12, 12);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(146, 37);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
+			this.bAutostart.Location = new System.Drawing.Point(12, 12);
+			this.bAutostart.Name = "bAutostart";
+			this.bAutostart.Size = new System.Drawing.Size(130, 62);
+			this.bAutostart.TabIndex = 0;
+			this.bAutostart.Text = "bAutostart";
+			this.bAutostart.UseVisualStyleBackColor = true;
+			this.bAutostart.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// FrmSettings
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(282, 255);
-			this.Controls.Add(this.button1);
+			this.ClientSize = new System.Drawing.Size(301, 103);
+			this.Controls.Add(this.bAutostart);
 			this.Name = "FrmSettings";
 			this.Text = "frmSettings";
+			this.Load += new System.EventHandler(this.FrmSettings_Load);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button bAutostart;
 	}
 }
