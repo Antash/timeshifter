4d04f475ffe259ff1c9fabf0fa772fa746036572
Anton Ashmarin <aashmarin@gmail.com>
2011-04-02 19:18:29 +0000

    prototype WIP

--- TimeShifterProto/tsCore/Classes/UserActLogStructure.cs
+++ TimeShifterProto/tsCore/Classes/UserActLogStructure.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows.Forms;
 
 namespace tsCore.Classes
