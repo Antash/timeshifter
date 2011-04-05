c3090c7684df42ff9362d4c0a63d9324b75591d9
anton <anton@104cdb4f-c8c8-40c2-b96b-405fcb135c29>
2011-04-05 09:08:25 +0000

    icon capture added

--- trunk/TimeShifterProto/tsWin/WinApiWrapper.cs
+++ trunk/TimeShifterProto/tsWin/WinApiWrapper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -8,37 +10,98 @@ namespace tsWin
 	{
 		private const int BuffLen = 100;
 
-	    [DllImport("user32.dll")]
+		[DllImport("user32.dll")]
 		private static extern IntPtr GetForegroundWindow();
 
-        [DllImport("user32.dll")]
+		[DllImport("user32.dll")]
 		private static extern int GetWindowThreadProcessId(IntPtr hWnd, ref int pid);
 
 		[DllImport("user32.dll")]
 		private static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
 
-        public static string GetActWindowProcName()
-        {
-			return System.Diagnostics.Process.GetProcessById(GetActWindowPID()).ProcessName;
-        }
+		[DllImport("shell32.dll")]
+		private static extern uint ExtractIconEx(string szFileName,
+										int nIconIndex,
+										IntPtr[] phiconLarge,
+										IntPtr[] phiconSmall,
+										uint nIcons);
 
-		public static string GetActWindowTitle()
+		[DllImport("user32.dll")]
+		private static extern int DestroyIcon(IntPtr hIcon);
+
+		internal static string GetWindowProcName(int pid)
 		{
-			IntPtr hwnd = GetForegroundWindow();
-			if (hwnd == (IntPtr)0)
-				return "no active window";
-			var sb = new StringBuilder(BuffLen);
-			GetWindowText(hwnd, sb, BuffLen);
-			return sb.ToString();
+			return pid != 0 ? Process.GetProcessById(pid).ProcessName : string.Empty;
 		}
 
-		public static int GetActWindowPID()
+		internal static string GetProcExecutablePath(string pName)
+		{
+			Process[] p = Process.GetProcessesByName(pName);
+			string path = string.Empty;
+			if (p.Length > 0)
+				path = p[0].MainModule.FileName;
+			return path;
+		}
+
+		internal static string GetProcExecutablePath(int pid)
+		{
+			return pid != 0 ? Process.GetProcessById(pid).MainModule.FileName : string.Empty;
+		}
+
+		internal static string GetWindowTitle(int pid)
+		{
+			return pid != 0 ? Process.GetProcessById(pid).MainWindowTitle : string.Empty;
+			//Old style function
+			//IntPtr hwnd = GetForegroundWindow();
+			//if (hwnd == (IntPtr)0)
+			//    return "no active window";
+			//var sb = new StringBuilder(BuffLen);
+			//GetWindowText(hwnd, sb, BuffLen);
+			//return sb.ToString();
+		}
+
+		internal static int GetActWindowPID()
 		{
 			int pid = 0;
 			IntPtr hwnd = GetForegroundWindow();
 			if (hwnd != (IntPtr)0)
 				GetWindowThreadProcessId(hwnd, ref pid);
 			return pid;
+		}
+
+		public static Icon ExtractIconFromExe(string file, bool isLarge)
+		{
+			var hDummy = new[] { IntPtr.Zero };
+			var hIconEx = new[] { IntPtr.Zero };
+			try
+			{
+				uint readIconCount = isLarge ?
+									ExtractIconEx(file, 0, hIconEx, hDummy, 1) :
+									ExtractIconEx(file, 0, hDummy, hIconEx, 1);
+
+				if (readIconCount > 0 && hIconEx[0] != IntPtr.Zero)
+				{
+					var extractedIcon = (Icon)Icon.FromHandle(hIconEx[0]).Clone();
+					return extractedIcon;
+				}
+				else
+					return null;
+			}
+			catch (Exception ex)
+			{
+				throw new ApplicationException("Could not extract icon", ex);
+			}
+			finally
+			{
+				// release resources
+				foreach (IntPtr ptr in hIconEx)
+					if (ptr != IntPtr.Zero)
+						DestroyIcon(ptr);
+
+				foreach (IntPtr ptr in hDummy)
+					if (ptr != IntPtr.Zero)
+						DestroyIcon(ptr);
+			}
 		}
 	}
 }
