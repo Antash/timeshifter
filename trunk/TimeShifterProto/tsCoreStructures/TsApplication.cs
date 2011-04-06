846eaba9382643370798a1ce15bf8cdf8eb7d397
anton <anton@104cdb4f-c8c8-40c2-b96b-405fcb135c29>
2011-04-06 15:10:49 +0000

    structure added

---
+++ trunk/TimeShifterProto/tsCoreStructures/TsApplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace tsCoreStructures
+{
+	class TsApplication
+	{
+		public string Name { get; set; }
+		public Icon SmallIcon { get; set; }
+		public Icon LargeIcon { get; set; }
+
+		public TsApplication()
+		{
+		}
+	}
+}
