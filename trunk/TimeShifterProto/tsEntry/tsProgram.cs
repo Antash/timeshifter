3ece1e45536da792114d606a834d78856dc41ad4
anton <anton@104cdb4f-c8c8-40c2-b96b-405fcb135c29>
2011-04-06 14:58:20 +0000

    add wpf application

--- trunk/TimeShifterProto/tsEntry/tsProgram.cs
+++ trunk/TimeShifterProto/tsEntry/tsProgram.cs
@@ -15,34 +15,7 @@ namespace tsEntry
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 
-			if (args.Length > 0)
-				
-			Application.Run(new WinFormsApplicationContext());
-		}
-	}
-
-	class WpfApplicationContext : ApplicationContext
-	{
-		public WpfApplicationContext()
-		{
-			//TODO : add Wpf UI handling
-		}
-	}
-
-	class WinFormsApplicationContext : ApplicationContext
-	{
-		private readonly FrmTray _mainForm;
-
-		public WinFormsApplicationContext()
-		{
-			_mainForm = new FrmTray();
-			_mainForm.FormClosed += MainFormFormClosed;
-			_mainForm.Show();
-		}
-
-		void MainFormFormClosed(object sender, FormClosedEventArgs e)
-		{
-			ExitThread();
+			Application.Run(new FrmTray());
 		}
 	}
 }
