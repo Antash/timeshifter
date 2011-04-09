81daac49c904f9850c4af54708fbf632c5c18677
anton <anton@104cdb4f-c8c8-40c2-b96b-405fcb135c29>
2011-04-09 08:31:38 +0000

    assembly info fixed

--- trunk/TimeShifterProto/tsEntry/Properties/AssemblyInfo.cs
+++ trunk/TimeShifterProto/tsEntry/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("tsEntry")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("TimeShifter")]
 [assembly: AssemblyProduct("tsEntry")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright © TimeShifter 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("0.1.*")]
+//[assembly: AssemblyFileVersion("0.1.*")]
