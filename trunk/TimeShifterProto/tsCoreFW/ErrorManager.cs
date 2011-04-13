16640d278d9e03675eb52fb5a041ca9f29e0ee2e
anton <anton@104cdb4f-c8c8-40c2-b96b-405fcb135c29>
2011-04-13 08:02:39 +0000

    fiz log

--- trunk/TimeShifterProto/tsCoreFW/ErrorManager.cs
+++ trunk/TimeShifterProto/tsCoreFW/ErrorManager.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace tsCoreFW
 {
 	public class ErrorManager
 	{
+		[Serializable]
+		struct ErrorLogItem
+		{
+			public DateTime Time { get; set; }
+			public string Assembly { get; set; }
+			public string Msg { get; set; }
+
+			public ErrorLogItem(DateTime time, string assm, string message) : this()
+			{
+				Time = time;
+				Assembly = assm;
+				Msg = message;
+			}
+		}
+
 		private static volatile ErrorManager _instance;
 		private static readonly object SyncRoot = new Object();
 
@@ -27,19 +45,24 @@ namespace tsCoreFW
 			}
 		}
 
+		StreamWriter _memoryStream;
 		protected ErrorManager()
 		{
-			ShowErrors = true;
+			ShowErrors = false;
+			
 		}
 
 		private FrmErr _errForm;
+		private const string Path = "err.log";
 		public bool ShowErrors { get; set; }
 
 		public void RiseError(string errMsg)
 		{
 			_errForm = new FrmErr();
 			_errForm.Init(errMsg);
-			
+			_memoryStream = new StreamWriter(Path, true);
+			_memoryStream.WriteLine(DateTime.Now + " " + errMsg);
+			_memoryStream.Close();
 			if (ShowErrors)
 			{
 				new Thread(() => _errForm.ShowDialog()).Start();
@@ -50,7 +73,9 @@ namespace tsCoreFW
 		{
 			_errForm = new FrmErr();
 			_errForm.Init(errModule, errMsg);
-
+			_memoryStream = new StreamWriter(Path, true);
+			_memoryStream.WriteLine(DateTime.Now + " " + errModule + " " + errMsg);
+			_memoryStream.Close();
 			if (ShowErrors)
 			{
 				new Thread(() => _errForm.ShowDialog()).Start();
