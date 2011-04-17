405682dd47ebff477de28a153b2d35229aa4fd1e
Anton Ashmarin <aashmarin@gmail.com>
2011-04-17 06:57:39 +0000

    comments added

--- TimeShifterProto/tsCoreStructures/TsTask.cs
+++ TimeShifterProto/tsCoreStructures/TsTask.cs
@@ -5,35 +5,74 @@ using tsCoreStructures.Base;
 
 namespace tsCoreStructures
 {
+	/// <summary>
+	/// This class represens structute of task
+	/// </summary>
 	public class TsTask : TsBaseStruct, IEquatable<TsTask>
 	{
+		/// <summary>
+		/// Table contains class data structure
+		/// </summary>
 		protected static DataTable StructTable;
 
+		/// <summary>
+		/// Unique task id
+		/// </summary>
 		[DataBaseColumn(true)]
 		public int Id { get; set; }
 
+		/// <summary>
+		/// Task name
+		/// </summary>
 		[DataBaseColumn]
 		public string TaskName { get; set; }
 
+		/// <summary>
+		/// Time when task started
+		/// </summary>
 		[DataBaseColumn]
 		public DateTime BeginTime { get; set; }
 
+		/// <summary>
+		/// Time when the task ended
+		/// </summary>
+		[DataBaseColumn]
 		public DateTime EndTime { get; set; }
 
+		/// <summary>
+		/// Planned time to finish task
+		/// </summary>
+		[DataBaseColumn]
 		public TimeSpan PlanTimeToSpend { get; set; }
 
+		/// <summary>
+		/// Actual finish time
+		/// </summary>
+		[DataBaseColumn]
 		public TimeSpan ActualTimeToSpend { get; set; }
 
+		/// <summary>
+		/// Short description of the task
+		/// </summary>
 		[DataBaseColumn]
 		public string Discription { get; set; }
 
-		public List<TsApplication> AssignedApplications { get; set; }
+		/// <summary>
+		/// Applications which are used in this task
+		/// </summary>
+		public IEnumerable<TsApplication> AssignedApplications { get; set; }
 
+#region constructors
+
+		/// <summary>
+		/// Creates new instance of TsApplication
+		/// </summary>
 		public TsTask()
 		{
 		}
 
 		public TsTask(string taskName, string taskDiscription, DateTime ts)
+			: this()
 		{
 			TaskName = taskName;
 			Discription = taskDiscription;
@@ -41,28 +80,47 @@ namespace tsCoreStructures
 		}
 
 		public TsTask(int id, string taskName, string taskDiscription, DateTime ts)
+			: this(taskName, taskDiscription, ts)
 		{
 			Id = id;
-			TaskName = taskName;
-			Discription = taskDiscription;
-			BeginTime = ts;
 		}
 
+#endregion
+
+		/// <summary>
+		/// Class comparer
+		/// </summary>
+		/// <param name="other">Other class copy</param>
+		/// <returns>Returns true if copies are equals</returns>
 		public bool Equals(TsTask other)
 		{
 			return TaskName == other.TaskName;
 		}
 
+		/// <summary>
+		/// Converts class instance to data row
+		/// </summary>
+		/// <returns>Data row contains class copy</returns>
+		/// <remarks>
+		/// You must to call BuildDataStructure() before calling this method
+		/// in order to create class data structure
+		/// </remarks>
 		public DataRow ToDataRow()
 		{
 			return ToDataRow(StructTable);
 		}
 
+		/// <summary>
+		/// Builds class table structure
+		/// </summary>
+		/// <returns>Ready-to-use in DAL DataTable</returns>
 		public override DataTable BuildDataStructure()
 		{
 			return StructTable = base.BuildDataStructure();
 		}
 	}
+
+#region delegates & event args
 
 	public delegate void NewTaskHandler(object sender, NewTaskHandlerArgs args);
 
@@ -75,4 +133,7 @@ namespace tsCoreStructures
 			Task = task;
 		}
 	}
+
+#endregion
+
 }
--- TimeShifterProto/tsCoreStructures/TsWindow.cs
+++ TimeShifterProto/tsCoreStructures/TsWindow.cs
@@ -8,6 +8,6 @@ namespace tsCoreStructures
 		public string Text { get; set; }
 		public DateTime OpenTime { get; set; }
 		public DateTime CloseTime { get; set; }
-		public List<UserActLogStructure> UserActSnapshots { get; set; }
+		public IEnumerable<UserActLogStructure> UserActSnapshots { get; set; }
 	}
 }
